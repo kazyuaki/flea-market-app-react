c25a1d604b75826c6c0535de08f1ce5e59de70ee
kazyuaki <kazuyama195@gmail.com>
2026-04-04 16:03:06 +0900

    feat(api): 商品一覧APIのルートを追加

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,9 +1,13 @@
 <?php
 
+use App\Http\Controllers\Api\ItemController;
+use App\Models\Item;
 use Illuminate\Support\Facades\Route;
 
-Route::get('test', function () {
-    return response()->json([
-        'message' => 'API OK'
-    ]);
-});
+// Route::get('test', function () {
+//     return response()->json([
+//         'message' => 'API OK'
+//     ]);
+// });
+
+Route::get('/items', [ItemController::class, 'index']);
