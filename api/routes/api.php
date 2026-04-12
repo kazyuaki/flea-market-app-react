341ea63a3ae58645da4a19ac44e58098efce510a
kazyuaki <kazuyama195@gmail.com>
2026-04-12 14:54:38 +0900

    feat: Fortify + Sanctum 認証基盤を導入

--- api/routes/api.php
+++ api/routes/api.php
@@ -6,16 +6,28 @@ use App\Http\Controllers\Api\Item\GetItemListController;
 use App\Http\Controllers\Api\Item\GetItemDetailController;
 use App\Http\Controllers\Api\Item\StoreCommentController;
 use App\Http\Controllers\Api\Purchase\GetPurchaseItemController;
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Route;
 
-/*** 商品関連 ***/
-Route::get('/items', GetItemListController::class);
-Route::get('/items/{id}', GetItemDetailController::class);
-Route::post('/items/{item}/comments', StoreCommentController::class);
+/*** ユーザー取得（これ追加）***/
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
 
-/**** 購入関連 ****/
-Route::get(('/purchase/{item_id}'), GetPurchaseItemController::class);
+/*** 認証関連 ***/
+Route::middleware('auth:sanctum')->group(function () {  
+    
 
-/**** 配送先関連 ****/
-Route::get('/purchase/address/{item_id}', GetAddressController::class);
-Route::post('/purchase/address', UpdateAddressController::class);
+    /*** 商品関連 ***/
+    Route::get('/items', GetItemListController::class);
+    Route::get('/items/{id}', GetItemDetailController::class);
+    Route::post('/items/{item}/comments', StoreCommentController::class);
+    
+    /**** 購入関連 ****/
+    Route::get(('/purchase/{item_id}'), GetPurchaseItemController::class);
+    
+    /**** 配送先関連 ****/
+    Route::get('/purchase/address/{item_id}', GetAddressController::class);
+    Route::post('/purchase/address', UpdateAddressController::class);
+});
+
