e5b5f2a96a1b792c2048a8c626fc3aaaa02c672c
kazyuaki <kazuyama195@gmail.com>
2026-05-02 20:59:42 +0900

    ✨ Feat: Stripe決済ライブラリと設定を追加

--- api/config/services.php
+++ api/config/services.php
@@ -35,4 +35,13 @@ return [
         ],
     ],
 
+    'stripe' => [
+        'key' => env('STRIPE_KEY'),
+        'secret' => env('STRIPE_SECRET'),
+    ],
+
+    'frontend' => [
+        'url' => env('FRONTEND_URL', 'http://localhost:5173'),
+    ],
+
 ];
