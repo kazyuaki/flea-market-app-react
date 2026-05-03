acdd2a9d270a17c5b9d024f38ed9ae7926226996
kazyuaki <kazuyama195@gmail.com>
2026-05-03 15:19:30 +0900

    購入手続き画面をレスポンシブ対応

--- node/src/components/Layouts/PurchaseLayout.tsx
+++ node/src/components/Layouts/PurchaseLayout.tsx
@@ -12,9 +12,9 @@ export const PurchaseLayout = ({ main, sidebar }: PurchaseLayoutProps) => {
     <>
       <Header />
       <div className="bg-gray-100 min-h-screen py-10">
-        <div className="max-w-[1000px] mx-auto flex gap-10">
-          <div className="flex-1">{main}</div>
-          <div className="w-[300px]">{sidebar}</div>
+        <div className="mx-auto flex w-full max-w-[1400px] flex-col gap-8 px-6 text-lg lg:flex-row lg:gap-10 lg:px-10 xl:text-xl 2xl:px-16">
+          <div className="min-w-0 flex-1">{main}</div>
+          <div className="w-full lg:w-[320px] xl:w-[360px]">{sidebar}</div>
         </div>
       </div>
     </>
