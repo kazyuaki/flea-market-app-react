762714339a49d9828c2d3ea6d413e08d0dc15a16
kazyuaki <kazuyama195@gmail.com>
2026-05-03 14:05:52 +0900

    feat: show sold state on items

--- node/src/components/Item/ItemImage.tsx
+++ node/src/components/Item/ItemImage.tsx
@@ -2,14 +2,15 @@ import noimage from '../../assets/noimage.png'
 type Props = {
   src?: string
   alt: string
+  className?: string
 }
 
-export default function ItemImage({ src, alt }: Props) {
+export default function ItemImage({ src, alt, className }: Props) {
   return (
     <img
       src={src || noimage}
       alt={alt}
-      className="w-[400px] h-[400px] object-cover bg-gray-200"
+      className={`w-[400px] h-[400px] object-cover bg-gray-200 ${className || ""}`}
     />
   )
 }
